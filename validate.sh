d2fa891347fd19a079f129847ceaaaf1235c387c
Wojdak <jakub.wojdak11@gmail.com>
2024-02-12 19:24:31 +0100

    .

--- validate.sh
+++ validate.sh
@@ -8,14 +8,6 @@ fi
 
 container_ip=$1
 
-# Check if the container is running
-if docker ps -f name=hello-world-container | grep -q hello-world-container; then
-    echo "Container is running"
-else
-    echo "Container is not running"
-    exit 1
-fi
-
 # Check if the container endpoint is reachable
 response=$(curl -s -o /dev/null -w "%{http_code}" http://$container_ip:8080)
 if [ "$response" == "200" ]; then
